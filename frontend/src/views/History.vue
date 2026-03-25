1be2204e8d096505d03458cf8b797eec86d22645
Diamondcoder2004 <almaz_sabitov04@mail.ru>
2026-03-25 12:44:45 +0500

    ui: иконка крестика на кнопке сброса фильтров в истории

--- frontend/src/views/History.vue
+++ frontend/src/views/History.vue
@@ -45,8 +45,7 @@
           />
           <button @click="resetFilters" class="reset-filters-btn" title="Сбросить фильтры">
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-              <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 12"/>
-              <path d="M3 3v9h9"/>
+              <path d="M18 6L6 18M6 6l12 12"/>
             </svg>
           </button>
         </div>
